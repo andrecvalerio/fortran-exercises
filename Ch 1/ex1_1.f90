28db407993fa51232b1ac03e21679abd607648b1
André Cordeiro Valério <b34832@fgv.edu.br>
2020-01-27 15:34:13 -0300

    Update code

--- Ch 1/ex1_1.f90
+++ Ch 1/ex1_1.f90
@@ -1,16 +1,18 @@
 program exe1_1
+    ! This exercise ask you to write a program that reads two scalars of type real*8 from the console
+    ! and should compute sum, difference, multiplication and division of these two scalars
+    
     implicit none
-    ! real*8 :: x, y, sum , diff, times, ratio
-    integer :: x, y, sum, diff, times, ratio 
+    real*8 :: x, y, sum , diff, times, ratio
 
     write(*,*) "Type any two numbers:"
     read(*,*) x, y
+
 
     sum = x + y
     diff = x - y
     times = x * y
     ratio = x / y
 
-    !write(*,'(f12.2, 2x, f12.2, 2x, f12.2, 2x, f12.2)') sum, diff, times, ratio
-    write(*,'(i2, 2x, i2, 2x, i2, 2x, i2)') sum, diff, times, ratio
+    write(*,'(f12.2, 2x, f12.2, 2x, f12.2, 2x, f12.2)') sum, diff, times, ratio
 end program 
